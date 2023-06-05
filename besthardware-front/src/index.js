1b1689fca8174d907faf661e0c3887a95e9a5d46
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-06-05 11:17:29 -0300

    Feat: Created Home/About/Catalog react routes

--- besthardware-front/src/index.js
+++ besthardware-front/src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './view/home';
+import About from './view/about';
+import Catalog from './view/catalog';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/catalogo",
+    element: <Catalog/>,
+  },
+  {
+    path: "/sobre",
+    element: <About/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
