bf3b9b5d5bdd08ca8b56aea08787c60dab03544c
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-06-06 09:09:51 -0300

    Feat: Created POST route

--- besthardware-back/index.js
+++ besthardware-back/index.js
@@ -1,11 +1,14 @@
+
+var bodyParser = require('body-parser')
 var fs = require('fs');
 // json file with the data
 var data = fs.readFileSync('products.json');
-  
+
 var elements = JSON.parse(data);
 const express = require("express");
 const app = express();
-  
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()) 
 // To solve the cors issue
 const cors = require('cors');
    
@@ -28,7 +31,35 @@ app.get('/elements/:element/', (req, res) => {
             status:"Not Found"
         }
     }
-       
+    
     res.send(reply);
 });
-  
+
+app.get('/test', (req, res, next) => {
+
+    elements.push({
+        "test": {
+            "brand": "Corsair",
+            "capacity": "8gb",
+            "imgPath": "NoImg",
+            "categories": ["memram", "8gb", "corsair"]
+        }
+    })
+    console.log(elements)
+    fs.writeFile("products.json", JSON.stringify(elements), function (err, result){
+        if(err) console.log('erro', err)
+    })
+
+    //elements.push({"name": "Tony", "age": "99"})
+
+    res.send('tested')
+})
+
+app.post('/elements/', (req, res, next) => {
+    elements.push(req.body)
+    fs.writeFile("products.json", JSON.stringify(elements), (err, result) => {
+        if (err) console.log('Error: ', err)
+        console.log(err)
+    })
+    res.status(201).json({message: "Created! :D"})
+})
